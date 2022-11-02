39a73d37acead061ad2ee9616341878ea67b44a1
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-11-02 09:35:04 -0400

    fix

--- src/components/landing/Home.js
+++ src/components/landing/Home.js
@@ -3,20 +3,19 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 // import { landingPage } from '../../utils/data'
 const image =
-  'https://res.cloudinary.com/inam6530/image/upload/v1667131704/inamwebsolutions/Untitled_design_2_atlc1q.svg'
+  'https://res.cloudinary.com/inam6530/image/upload/v1667394769/inamwebsolutions/Untitled_design_s5azna.svg'
 
 const Home = ({ landingPage }) => {
   console.log(landingPage)
   return (
     <Wrapper>
       <div className='box box-mobile'>
-        <div className='mobile-container'>
-          <h1>{landingPage.heading}</h1>
-          <p>{landingPage.paragraph}</p>
-          <Link to={landingPage.path} className='btn'>
-            {landingPage.buttonTitle}
-          </Link>
-        </div>
+        <h1>{landingPage.heading}</h1>
+        <img src={image} alt='computer' />
+        <p>{landingPage.paragraph}</p>
+        <Link to={landingPage.path} className='btn'>
+          {landingPage.buttonTitle}
+        </Link>
       </div>
       <div className='box box-desktop'>
         <img src={landingPage.image} alt='computerPicture' />
@@ -34,19 +33,16 @@ const Wrapper = styled.div`
     font-size: 1.2rem;
   }
   .box {
-    height: 100vh;
+    min-height: 100vh;
   }
   .box-mobile {
     display: grid;
     padding: 2rem;
-    align-items: center;
-    background-image: url(${image});
-    background-repeat: no-repeat;
-    background-size: cover;
-    width: 100%;
-    overflow: hidden;
-    .mobile-container {
-      overflow: hidden;
+    align-content: center;
+    justify-items: center;
+
+    img {
+      max-width: 300px;
     }
   }
   .box-desktop {
@@ -55,25 +51,23 @@ const Wrapper = styled.div`
       width: 90%;
     }
   }
-  @media (min-width: 620px) {
+  @media (min-width: 992px) {
+    grid-template-columns: 1.5fr 1fr;
+    .box-desktop {
+      display: grid;
+      align-content: end;
+    }
     .box-mobile {
+      padding: 4rem;
       background: linear-gradient(
         90deg,
         rgba(100, 116, 139, 1) 0%,
         rgba(226, 232, 240, 1) 94%,
         rgba(248, 250, 252, 1) 100%
       );
-    }
-  }
-  @media (min-width: 720px) {
-    grid-template-columns: 1.5fr 1fr;
-    .box-desktop {
-      display: grid;
-
-      align-content: end;
-    }
-    .box-mobile {
-      padding: 4rem;
+      img {
+        display: none;
+      }
     }
   }
 `
