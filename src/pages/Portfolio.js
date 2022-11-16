e6015b215504d96c74db10c0d2ede7f1fcece211
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-11-16 11:38:03 -0500

    update

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 import styled from 'styled-components'
 import LandingPortfolio from '../components/landing/LandingPortfolio'
 
 const Portfolio = () => {
   return (
-    <Wrapper>
-      <LandingPortfolio />
-    </Wrapper>
+    <>
+      <Helmet>
+        <title>Portfolio Projects</title>
+        <meta
+          name='description'
+          content='Our Website Design and layout work almost every device.And they are very fast responsive.'
+        />
+        <link rel='canonical' href='/' />
+      </Helmet>
+      <Wrapper>
+        <LandingPortfolio />
+      </Wrapper>
+    </>
   )
 }
 
